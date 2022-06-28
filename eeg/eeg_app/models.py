06806642aef775d2f13b3a1c62382ef26558e28d
Jonathan <me@example.com>
2022-06-27 21:22:11 -0400

    Model fixes and first migration

--- eeg/eeg_app/models.py
+++ eeg/eeg_app/models.py
@@ -3,23 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Facility(models.Model):
-    name = models.CharField()
-    country = models.CharField()
-    postalCode = models.CharField()
+    name = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
+    postalCode = models.CharField(max_length=10)
 
 class Subject(models.Model):
-    identifier = models.CharField()
-    facilityId = models.ForeignKey(Facility, null=True)
-    diagnosis = models.CharField()
+    identifier = models.TextField()
+    facilityId = models.ForeignKey(Facility, null=True, on_delete=models.DO_NOTHING)
+    diagnosis = models.TextField()
 
 class File(models.Model):
     subjectId = models.ForeignKey(Subject, on_delete=models.CASCADE)
-    fileName = models.CharField()
+    fileName = models.TextField()
 
 class Channel(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=20)
     unique = models.BooleanField()
-    fileNameId = models.ForeignKey(File, null=True)
+    fileNameId = models.ForeignKey(File, null=True, on_delete=models.DO_NOTHING)
 
 class Signal(models.Model):
     fileId = models.ForeignKey(File, on_delete=models.CASCADE)
